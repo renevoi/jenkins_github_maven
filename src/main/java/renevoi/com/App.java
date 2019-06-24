990176b1a7f461c3156fb828afba52b4d485d37b
rene <rene.catapang@gmail.com>
2019-06-25 00:24:56 +0800

    extracted a method that print the hello world 3 times

--- src/main/java/renevoi/com/App.java
+++ src/main/java/renevoi/com/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+         print();
+    }
+
+    private static void print() {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World AGAIN!" );
         System.out.println( "Hello World AGAIN and AGAIN!" );
