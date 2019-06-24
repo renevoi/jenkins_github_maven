c03388cca074f8c1a9a5f3aa77052f72b9515eaf
rene <rene.catapang@gmail.com>
2019-06-25 04:19:57 +0800

    format code

--- src/main/java/renevoi/com/App.java
+++ src/main/java/renevoi/com/App.java
@@ -2,23 +2,16 @@ package renevoi.com;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        // change something
-         print();
-        System.out.printf("HAHAHAHAHAAAHAHAHA");
-        System.out.printf("LALALALALALALALALALALALALA");
-        System.out.println("sksdjflkjsdlkfj");
+public class App {
+    public static void main(String[] args) {
+        print();
 
     }
 
     private static void print() {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World AGAIN!" );
-        System.out.println( "Hello World AGAIN and AGAIN!" );
+        System.out.println("Hello World!");
+        System.out.println("Hello World AGAIN!");
+        System.out.println("Hello World AGAIN and AGAIN!");
     }
 }
