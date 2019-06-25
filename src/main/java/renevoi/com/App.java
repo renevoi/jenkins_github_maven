18db461451d8e07383557a1dfce008038a63f065
rene <rene.catapang@gmail.com>
2019-06-26 01:06:55 +0800

    last commit

--- src/main/java/renevoi/com/App.java
+++ src/main/java/renevoi/com/App.java
@@ -2,16 +2,23 @@ package renevoi.com;
 
 /**
  * Hello world!
+ *
  */
-public class App {
-    public static void main(String[] args) {
-        print();
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // change something
+         print();
+        System.out.printf("HAHAHAHAHAAAHAHAHA");
+        System.out.printf("LALALALALALALALALALALALALA");
+        System.out.println("sksdjflkjsdlkfj");
 
     }
 
     private static void print() {
-        System.out.println("Hello World!");
-        System.out.println("Hello World AGAIN!");
-        System.out.println("Hello World AGAIN and AGAIN!");
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World AGAIN!" );
+        System.out.println( "Hello World AGAIN and AGAIN!" );
     }
 }
