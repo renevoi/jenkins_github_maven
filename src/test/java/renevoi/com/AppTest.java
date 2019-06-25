eab9a79e2b33ec1999168e95fa946c6b4f9f57f8
rene <rene.catapang@gmail.com>
2019-06-26 01:54:03 +0800

    alter test

--- src/test/java/renevoi/com/AppTest.java
+++ src/test/java/renevoi/com/AppTest.java
@@ -1,8 +1,8 @@
 package renevoi.com;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -15,6 +15,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue( false );
     }
 }
